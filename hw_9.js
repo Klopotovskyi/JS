cc6bfcb5889502d91efbab4fe9bf5c8d9151c780
Klopotovskyi <patola@ukr.net>
2019-01-18 16:42:47 +0200

    common strukture of request

--- hw_9.js
+++ hw_9.js
@@ -2,18 +2,22 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-let limitNumber = setTimeout(getRandomInt(1, 11), 1000);
-let pageNumber = setTimeout(getRandomInt(1, 11), 2000);
 
-function getPost(callback) {
+function getPagesProp (){
+    let limitNumber = getRandomInt(1, 11);
+    let pageNumber = getRandomInt(1, 11);
+}
+
+
+function getPost() {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status === 200){
-          callback (this.responseText);
-      }
-      xhttp.open('GET', 'http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}');
+        if (this.readyState === 4 && this.status === 200) {
+            console.warn(JSON.parse(this.responseText));
+        }
+    };
+      xhttp.open("GET", "http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}");
       xhttp.send();
-    }
 }
 getPost(function(callback) {
     console.log(JSON.parse(this.responseText));
