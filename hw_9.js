084c339345290726bfb490e5a9c63f5ea534d4b2
Klopotovskyi <patola@ukr.net>
2019-01-22 16:56:13 +0200

    delay 2.5 sec with callback&update random

--- hw_9.js
+++ hw_9.js
@@ -3,16 +3,22 @@ document.addEventListener('DOMContentLoaded', function () {
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
     }
-    let limitNumber = setTimeout(getRandomInt(1, 11), 1000);
-    let pageNumber = setTimeout(getRandomInt(1, 11), 2000);
-    function getPost(callback) {
+    setTimeout(function () {
+        let limitNumber=getRandomInt(1, 11);
+        console.log(limitNumber)
+    }, 2000);
+    setTimeout(function () {
+        let pageNumber=getRandomInt(1, 11);
+        console.log(pageNumber)
+    }, 2000);
+   function getPost(callback) {
         let xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState === 4 && this.status === 200) {
                 callback(this.responseText);
             }
         };
-        xhttp.open("GET", "http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}");
+        xhttp.open('GET', 'http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}');
         xhttp.send();
     }
     setTimeout(function () {
