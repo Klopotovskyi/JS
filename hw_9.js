90f37c622f26beb29e0a38381acffc2074a334bd
Klopotovskyi <patola@ukr.net>
2019-01-22 15:57:47 +0200

    callback use

--- hw_9.js
+++ hw_9.js
@@ -1,28 +1,21 @@
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-
-function getPagesProp (){
-    let limitNumber = getRandomInt(1, 11);
-    let pageNumber = getRandomInt(1, 11);
-}
-
-
-function getPost() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            console.warn(JSON.parse(this.responseText));
-        }
-    };
-      xhttp.open("GET", "http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}");
-      xhttp.send();
-}
-getPost(function(callback) {
-    console.log(JSON.parse(this.responseText));
+'use strict';
+document.addEventListener('DOMContentLoaded', function () {
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+    let limitNumber = setTimeout(getRandomInt(1, 11), 1000);
+    let pageNumber = setTimeout(getRandomInt(1, 11), 2000);
+    function getPost(callback) {
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                callback(this.responseText);
+            }
+        };
+        xhttp.open("GET", "http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}");
+        xhttp.send();
+    }
+    setTimeout(getPost(function (responseText) {
+        console.log(JSON.parse(responseText));
+    }), 2500);
 })
-
-
-
-
