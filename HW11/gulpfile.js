e41a3600637845843fb080f49136434de19f353b
Klopotovskyi <patola@ukr.net>
2019-03-01 17:43:13 +0200

    to do list without classes

--- HW11/gulpfile.js
+++ HW11/gulpfile.js
@@ -1,25 +1,26 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+var gulp = require ('gulp');
+var sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const rewriteCSS = require('gulp-rewrite-css');
+var rewriteCSS = require('gulp-rewrite-css');
 
 sass.compiler = require('node-sass');
 
-gulp.task('sass', () => gulp.src('./src/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false,
-    }))
-    .pipe(gulp.dest('./src/css')));
+gulp.task('sass', function () {
+    return gulp.src('./src/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false}))
+        .pipe(gulp.dest('./src/css'))
+});
 
-gulp.task('my-rewrite', () => {
-    const dest = './dist/';
+gulp.task('my-rewrite', function() {
+    var dest = './dist/';
     return gulp.src('./static/css/*.css')
-        .pipe(rewriteCSS({ destination: dest }))
+        .pipe(rewriteCSS({destination:dest}))
         .pipe(gulp.dest(dest));
 });
 
-gulp.task('sass:watch', () => {
+gulp.task('sass:watch', function () {
     gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
